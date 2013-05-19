681065b803a849471aa87fafe8eb37654fb4c84c
Joshua Theissen <joshua@jtheissen.com>
2013-05-19 15:00:31 -0400

    Initial commit
    After tinkering for a day or two, i feel like it's okay for people to
    look at - still a great deal of footsteps to go back and cover up, but
    have at it.

---
+++ configure.js
@@ -0,0 +1,31 @@
+var setTheme = "Colors";
+	// Early stages of a theming system
+	// Current choices are "default", "Colors", and "Text"
+	// Colors is set to match the LS Squares Winterboard
+	// Text is coming soon, meant to use only text, no numbers.
+	
+	setColor = "#000",
+	// Hex Color eg. "#000"
+	
+	setOpacity = 1,
+	// Opacity of entire clock, from 0 (transparent) - 1 (opaque)
+	
+	setFont = "TGA-Regular",
+	// Font Family (Check iosfonts.com for choices based on device)
+	// "default" is Tex Gyre Adventor Regular
+	
+	set24hr = true,
+	// Use 24-hour Clock
+	
+	setFadeIn = 200,
+	// Fade in on refresh, in ms, set to 0 for no fade
+	
+	setTop = 20,
+	// Top Margin in px, 20 default for status bar
+	
+	setBlink = true,
+	// Blink ":" between Hr & Min
+	
+	setRefresh = 10;
+	// Number of Seconds between time updates
+
---
+++ js/runClock.js
@@ -0,0 +1,82 @@
+var dayNames = new Array(
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+);
+var monthNames = new Array(
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+);
+
+var setRefresh = setRefresh ? setRefresh*1000 : 30000;
+var now = new Date(); // Current Date (Client Side)
+function upDate () {
+	now = new Date();
+}
+setInterval("upDate()", 1000);
+function random (max)  { // Random Number
+	return Math.floor(Math.random()*(max+1));
+};
+function ldZ(num) { // Generates leading zeroes
+	var newNum = num < 10 ? "0" + num : num;
+	return newNum;
+}
+var hours = now.getHours ();
+hours = hours === 0 && !set24hr ? 12 : hours; // Eliminate 00:00 at midnight unless 24hr
+var hours12 = hours > 12 ? hours - 12 : hours;
+var use24 = set24hr ? hours : hours12; // 24hr Clock
+function setDate() { // Sets the date
+	$("#hour").html(ldZ(use24));
+	$("#minute").html(ldZ(now.getMinutes()));
+	$("#day span").html(dayNames[now.getDay()]);
+	$("#month span").html(monthNames[now.getMonth()]);
+	$("#date span").html(ldZ(now.getDate()));
+	$("#year span").html(now.getFullYear());
+};
+
+function blinky () {
+	$(".blinkme").fadeToggle(500, "linear")
+}
+
+function slabTextHeadlines() { // Set Text Full Width
+	$("h1").slabText();
+};
+
+function setConfig () { // Adjust CSS to preference via configure.js
+	switch (setTheme) {
+		case "Colors":
+			$("link.theme").attr("href","css/colorsTheme.css");
+			break;
+		default:	
+			//$("link.theme").attr("href","css/default.css");			
+			$("body").css('color', setColor);
+			$("#month").css('background', setColor);
+			$("html").css('font-family', setFont);
+			break;
+	}
+	$("body").css('opacity', setOpacity);
+	$("#time").css('padding-top', (setTop-20)+"px");
+};
+
+$(window).load(function() { // Run 
+	setDate();
+	setInterval("blinky()",500);
+	setConfig();
+	slabTextHeadlines();
+	$("body div").fadeTo(setFadeIn,1); // fade in, hides pre-js html
+	setInterval("setDate()", setRefresh);
+});
